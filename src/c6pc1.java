2b45ac3a03cbb040658be4400de2c0ca57ce30d6
tim <tlt31@uakron.edu>
2015-03-31 08:41:08 -0400

    create mutators

---
+++ src/c6pc1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by tim on 3/30/15.
+ */
+public class c6pc1 {
+
+    public static void main(String[] args){
+
+        Employee susan = new Employee("Susan Meyers", 47899, "Accounting", "Vice President");
+
+        System.out.println(susan.getDepartment());
+
+    }
+}
